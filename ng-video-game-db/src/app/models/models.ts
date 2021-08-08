799a29135f8850fa410501f890f008245c290c26
LeiscarT <leiscartp29@gmail.com>
2021-08-08 17:11:29 -0400

    Add some models to the application

--- ng-video-game-db/src/app/models/models.ts
+++ ng-video-game-db/src/app/models/models.ts
@@ -17,3 +17,33 @@ export interface Game{
 export interface APIResponse<T>{
     result: Array<T>;
 }
+
+interface Genre{
+    name:string;
+}
+
+interface ParentPlatform{
+    platform: {
+        name:string;
+    }
+}
+
+interface Publisher{
+    name:string;
+}
+
+interface Rating{
+    id: number;
+    count: number;
+    title: string;
+}
+
+interface Screenshot{
+    image: string;
+}
+
+interface Trailer{
+    data:{
+        max: string;
+    };
+}
